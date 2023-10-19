a159f2516d9360c88f9467453e92116d4be8d3f3
danish-05 <147092497+danish-05@users.noreply.github.com>
2023-10-19 11:24:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,168 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction(){
+    let menuBtn = document.getElementById("myNavMenu");
+
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+  window.onscroll = function() {headerShadow()};
+
+  function headerShadow() {
+    const navHeader =document.getElementById("header");
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+
+      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+      navHeader.style.height = "70px";
+      navHeader.style.lineHeight = "70px";
+
+    } else {
+
+      navHeader.style.boxShadow = "none";
+      navHeader.style.height = "90px";
+      navHeader.style.lineHeight = "90px";
+
+    }
+  }
+
+
+/* ----- TYPING EFFECT ----- */
+ let typingEffect = new Typed(".typedText",{
+    strings : ["Developer","Determined...","Creator"],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 1500
+ })
+
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+ const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 2000,
+        reset: true     
+ })
+
+/* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box',{interval: 200})
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header',{})
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
+
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+
+    }  else {
+
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+function validate(){
+  var username=document.getElementById("uname");
+  var password=document.getElementById("pass");
+
+  if(username .ariaValueMax.trim()==""|| password.ariaValueMax.trim()=="")
+  {
+    alert("NO Blank Values Allowed");
+    return false;
+  }
+  else{
+    true;
+  }
+}
+
+// <script>
+//         function validate() {
+//             var uname = document.getElementById("uname");
+//             var message = document.getElementById("comment");
+
+//             if (uname.value.trim() === "" || message.value.trim() === "") {
+//                 alert("Fields should not be blank");
+//                 return false;
+//             } else if (message.value.trim().length < 5) {
+//                 alert("Message is too short");
+//                 return false;
+//             } else {
+//                 return true;
+//             }
+//         }
+
+//         $("#submit-form").submit((e) => {
+//             e.preventDefault();
+//             if (validate()) {
+//                 $.ajax({
+//                     url: "https://script.google.com/macros/s/AKfycbw9MuikV4ScgXjR2KOq2hhf2oFaC9Rl3BFleul6SwovBnvqAPL2szxNnl7tC2pJ3qtxMA/exec",
+//                     data: $("#submit-form").serialize(),
+//                     method: "post",
+//                     success: function (response) {
+//                         alert("Form submitted successfully");
+//                         window.location.reload();
+//                     },
+//                     error: function (err) {
+//                         console.error(err);
+//                         alert("Something went wrong. Please check the console for details.");
+//                     }
+//                 });
+//             }
+//         });
+
+// </script>
